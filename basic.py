3597aff30dc5f5c0eb647b68552e2c0f4bb2fed4
kittinan365 <Kittinan.sa@rmuti.ac.th>
2024-07-04 12:00:08 +0700

    function

--- basic.py
+++ basic.py
@@ -1,24 +1,18 @@
-name = 'kittinan samaksaman'
-# print(6%2) #เอาค่าเศษ
-# print(2**4) #คูณกันตามจำนวน
-# print(100//9) #หาร9
-
-# x1 = 89 % 6
-# x1 += 5
-# x3 = x2 = x1
-# x2 /= 2
-# x2 = x2 / 2120
-
-# print(x1)#10
-# print(x2)#5.0
-# print(x3)#10
+def plus(num,num2) :
+    print ("บวก")
+    print (num+num2)
+def minus(num,num2) :
+    print ("ลบ")
+    print (num-num2)
+def multiply(num,num2) :
+    print ("คูณ")
+    print (num*num2)
+def divide(num,num2) :
+    print ("หาร")
+    print (num/num2)
 
 
-
-
-
-
-
-
-
-
+plus(5,10)
+minus(5,10)
+multiply(15,20)
+divide(40,20)
